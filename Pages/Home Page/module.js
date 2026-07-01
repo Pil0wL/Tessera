9c53888ad52ae797ef173f88e30e0cc6064970a6
Pil0wL <cadmonjaslibetario@gmail.com>
2026-07-02 02:04:32 +0800

    i want to live
    even if its for me, I added listeners for statistics, notable ones are total global submitted tickets, and per-user total submitted tickets

--- Pages/Home Page/module.js
+++ Pages/Home Page/module.js
@@ -1,12 +1,13 @@
 import { handleSignOut, getUserRole, changePreferredUsername } from "../.././local_modules/aws main.js";
 import { fetchAuthSession } from "https://esm.sh/@aws-amplify/auth";
+import { post } from "https://esm.sh/aws-amplify/api";
 
 // logout
 document.getElementById("logout").addEventListener("click", async () => {
   await handleSignOut("../.././index.html");
 });
 
-  
+
 const Button_GoToAdminPanel = document.getElementById("gotoadminpanel");
 Button_GoToAdminPanel.style.display = "none";
 const { user_presedence, highest_precedence_role } = await getUserRole();
@@ -16,8 +17,28 @@ if (user_presedence < 5) {
   Button_GoToAdminPanel.style.display = "flex";
 
   Button_GoToAdminPanel.addEventListener("click", () => {
-    window.location.href=".././Admin Panel/Admin Panel.html";
+    window.location.href = ".././Admin Panel/Admin Panel.html";
   });
 }
 
 changePreferredUsername("Super Cool User");
+
+try {
+  const session = await fetchAuthSession();
+  const token = session.tokens?.idToken?.toString();
+  const restOperation = post({
+    apiName: "Tessera-RestAPI",
+    path: "/Statistics/Tessera-BasicUser-LogActivity",
+    options: {
+      headers: {
+        Authorization: token
+      },
+      body: {}
+    }
+  });
+
+  const response = await restOperation.response;
+  console.log("Statistics | Successfully logged activity!", await response.body.json());
+} catch (error) {
+  console.error("Statistics | Failed to log activity", error.message);
+}
--- Pages/Landing Page/module.js
+++ Pages/Landing Page/module.js
@@ -3,7 +3,52 @@ import { getUser, delay } from "../.././local_modules/aws main.js";
 import { signIn, signInWithRedirect, fetchAuthSession } from "https://esm.sh/aws-amplify/auth";
 import { post } from "https://esm.sh/aws-amplify/api";
 
-const loginForm = document.getElementById('loginForm');
+
+const guestPrompt = document.getElementById("notloggedin");
+const alreadyLoggedInPrompt = document.getElementById("loggedin");
+
+guestPrompt.style.display = "none";
+alreadyLoggedInPrompt.style.display = "none";
+
+async function attemptCreationDatabaseEntry() {
+    let currentUser = await getUser();
+    if (!currentUser) {
+        return false; // no user is logged in; is a guest
+    }
+    try {
+        const session = await fetchAuthSession();
+        const token = session.tokens?.idToken?.toString(); 
+        const restOperation = post({
+            apiName: "Tessera-RestAPI",
+            path: "/Tessera-CreateUserDataEntry",
+            options: {
+                headers: {
+                    Authorization: token
+                },
+                body: {}
+            }
+        });
+
+        const response = await restOperation.response;
+        console.log("Successfully checked if the user has a data base entry or not!");
+
+        guestPrompt.style.display = "none";
+        alreadyLoggedInPrompt.style.display = "block";
+        return true;
+    } catch (error) {
+        console.error("database entry verification error:", error.message, "if it says its a network error, try checking the lambda to see if something crashed it");
+        return false;
+    }
+
+    
+}
+let checking_if_created = await attemptCreationDatabaseEntry();
+if (!checking_if_created) {
+    guestPrompt.style.display = "block";
+}
+
+
+const loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Stops the page from reloading
@@ -22,7 +67,7 @@ loginForm.addEventListener("submit", async (event) => {
 
         if (isSignedIn) {
             alert("Login Successful!");
-            window.location.href = "./Pages/Home Page/Home Page.html";
+            attemptCreationDatabaseEntry();
         } else if (nextStep.signInStep === "CONFIRM_SIGN_UP") {
             alert("Please confirm your email first.");
         }
@@ -43,34 +88,3 @@ googleBtn.addEventListener("click", async () => {
         console.error("Error starting Google login", error);
     }
 });
-
-document.getElementById("loggedin").style.display = "none"; // for the moment
-let currentUser = await getUser();
-while (currentUser) {
-    // this basically loops until it can confirm that there is an entry for userData for this account
-    try {
-        const session = await fetchAuthSession();
-        const token = session.tokens?.idToken?.toString(); 
-        const restOperation = post({
-            apiName: "Tessera-RestAPI",
-            path: "/Tessera-CreateUserDataEntry",
-            options: {
-                headers: {
-                    Authorization: token
-                },
-                body: {}
-            }
-        });
-
-        const response = await restOperation.response;
-        console.log("Success!", await response.body.json());
-
-        document.getElementById("notloggedin").style.display = "none";
-        document.getElementById("loggedin").style.display = "block";
-        break;
-    } catch (error) {
-        console.error("database entry verification error:", error.message, "if it says its a network error, try checking the lambda to see if something crashed it");
-    }
-
-    await delay(1000);
-}
